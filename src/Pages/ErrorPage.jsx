2b1e43ff748dd23f944f56fc0e59e68fb98369d1
Raheel Arfeen <raheelarfeen@gmail.com>
2025-08-09 09:59:18 +0600

    added dark mode in error page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -11,19 +11,19 @@ const ErrorPage = () => {
     }, []);
 
     return (
-        <div className='min-h-screen flex flex-col justify-between w-full bg-white transition-colors duration-300'>
+        <div className="min-h-screen flex flex-col justify-between w-full bg-white dark:bg-gray-900 transition-colors duration-300">
             <Header />
-            <div className='py-16'>
+            <div className="py-16">
                 <div className="flex flex-col items-center justify-center text-center px-4">
                     <Lottie animationData={Icon404} />
-                    <h2 className="text-2xl font-semibold text-gray-900">
+                    <h2 className="text-2xl font-semibold text-gray-900 dark:text-gray-100">
                         Page Not Found
                     </h2>
-                    <p className="text-gray-600 max-w-sm mb-6">
+                    <p className="text-gray-600 dark:text-gray-400 max-w-sm mb-6">
                         Oops! The page you're looking for seems to have wandered off.
                     </p>
-                    <Link to='/'>
-                        <button className="bg-blue-600 hover:bg-blue-700 text-white font-medium px-6 py-2 rounded-md transition-colors cursor-pointer">
+                    <Link to="/">
+                        <button className="bg-blue-600 dark:bg-blue-700 hover:bg-blue-700 dark:hover:bg-blue-800 text-white font-medium px-6 py-2 rounded-md transition-colors cursor-pointer">
                             Return to Home
                         </button>
                     </Link>
